c2a205f3022a3ff782cb2da24699bc32b59a0b8d
joshcarp <joshuacarpeggiani@gmail.com>
2020-10-12 18:10:22 +1100

    Use new api

--- rosterbot.go
+++ rosterbot.go
@@ -8,46 +8,48 @@ import (
 	"strings"
 	"time"
 
+	"cloud.google.com/go/pubsub"
 	"github.com/slack-go/slack"
-	"google.golang.org/api/pubsub/v1"
 )
 
 type server struct {
 	*slack.Client
 }
 
-type RosterPayload struct{
+type RosterPayload struct {
 	command
 	Channel string
 }
 
-func (r RosterPayload)toMap()map[string]string{
+func (r RosterPayload) toMap() map[string]string {
 	return map[string]string{
-		"channel": r.Channel,
+		"channel":   r.Channel,
 		"starttime": r.command.StartTime.String(),
-		"message": r.command.Message,
-		"users": strings.Join(r.command.Users, ", "),
+		"message":   r.command.Message,
+		"users":     strings.Join(r.command.Users, ", "),
 	}
 }
 
-func (r *RosterPayload)fromMap(m map[string]string){
+func (r *RosterPayload) fromMap(m map[string]string) {
 	r.Channel = m["channel"]
 	r.StartTime, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", m["starttime"])
 	r.Message = m["message"]
 	r.Users = strings.Split(m["users"], ", ")
 }
 
-func ServeHTTP(w http.ResponseWriter, r *http.Request){
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s := server{Client: nil}
 	s.ServeHTTP(w, r)
 }
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println(r)
+	log.Println(r.URL)
 	cmd, _ := slack.SlashCommandParse(r)
 	_, err := SlackCommandSubscribe(cmd)
-	if err != nil{
-		log.Println(err)
+	if err != nil {
+		log.Println("{"+err.Error()+"}")
+	}else{
+		log.Println("yay")
 	}
 
 }
@@ -56,15 +58,14 @@ func SlackCommandSubscribe(cmd slack.SlashCommand) (*pubsub.Subscription, error)
 	rosterbotCommand, _ := ParseCommand(cmd.Command)
 	payload := RosterPayload{command: rosterbotCommand, Channel: cmd.Command}
 	ctx := context.Background()
-	pubsubService, _ := pubsub.NewService(ctx)
-	return pubsubService.Projects.Subscriptions.Create(
-		payload.Channel,
-		&pubsub.Subscription{
-			Topic:"projects/joshcarp-installer/topics/slack",
-			PushConfig: &pubsub.PushConfig{
-				Attributes:      payload.toMap(),
-				PushEndpoint:    os.Getenv("PUSH_URL"),
-			}}).Do()
+	pubsubService, _ := pubsub.NewClient(ctx, "joshcarp-installer")
+	pubsubService.CreateSubscription(ctx, payload.Channel, pubsub.SubscriptionConfig{
+		Topic:               nil,
+		PushConfig:          pubsub.PushConfig{
+			Endpoint:             "projects/joshcarp-installer/topics/slack",
+			Attributes:           payload.toMap(),
+		},
+	})
 }
 
 func (s *server) SlackRespond(w http.ResponseWriter, r *http.Request) {
