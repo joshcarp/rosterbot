b220d988e5a759a7c2563c1eccc5445a343e87a9
Joshua Carpeggiani <32605850+joshcarp@users.noreply.github.com>
2020-10-17 16:57:05 +1100

    Update respond.go

--- roster/respond.go
+++ roster/respond.go
@@ -43,7 +43,7 @@ func (s Server) Respond(ctx context.Context, time2 time.Time) error {
 		snap.DataTo(&webhook)
 		message := payload.Message
 		if len(payload.Users) > 0{
-			message += " "+ payload.Users[payload.Time.Steps(payload.StartTime, time.Now())%len(payload.Users)-1]
+			message += " "+ payload.Users[(payload.Time.Steps(payload.StartTime, time.Now())-1)%(len(payload.Users))]
 		}
 		go slack.PostWebhookCustomHTTPContext(
 			ctx,
