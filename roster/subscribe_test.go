d5317c7adc6d28de56de2583e9bc8e85774bd91e
joshcarp <joshuacarpeggiani@gmail.com>
2020-10-27 12:56:26 +1100

    Add time tests

--- roster/subscribe_test.go
+++ roster/subscribe_test.go
@@ -3,42 +3,65 @@ package roster
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"github.com/joshcarp/rosterbot/database"
-	"github.com/slack-go/slack"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/joshcarp/rosterbot/database"
+	"github.com/slack-go/slack"
 )
 
-type test struct{
-	text string
-	teamid string
+type testcase struct {
+	text      string
+	teamid    string
 	channelid string
+	message   map[string]string
 }
-var tests []test{
 
+var tests = []testcase{
+
+	{
+		text:      `add "* * * * *" "message" @user1`,
+		teamid:    "team1",
+		channelid: "channel1",
+		message: map[string]string{"3:04PM":"message @user1"},
+	},
 }
 
 func TestFilter(t *testing.T) {
-	cmd := slack.SlashCommand{
-		TeamID:         "id",
-		ChannelID:      "foobar",
-		Text:           `add "* * * * *" "THis is a message" @joshuacarpeggiani `,
+	for _, test := range tests {
+		ctx := context.Background()
+		cmd := slack.SlashCommand{
+			TeamID:    test.teamid,
+			ChannelID: test.channelid,
+			Text:      test.text,
+		}
+		s, client := NewMockServer(test)
+		_, err := s.Enroll(ctx, "")
+		require.NoError(t, err)
+		_, _, err = s.Subscribe(ctx, cmd)
+		require.NoError(t, err)
+
+		for timestr, message := range test.message{
+			t1, err := time.Parse(time.Kitchen, timestr)
+			require.NoError(t, err)
+			err = s.Respond(ctx, t1)
+			require.NoError(t, err)
+			require.Equal(t, message, client.message)
+		}
 	}
-	s, client := NewMockServer()
-	s.Enroll(context.Background(), "1234")
-	a, b,  c := s.Subscribe(context.Background(), cmd)
-	s.Respond(context.Background(), time.Now())
-	fmt.Println(a, b, c)
-	fmt.Println(client)
+
 }
 
-func NewMockServer()(Server, *testSlackClient){
-	testClient := testSlackClient{}
+func NewMockServer(test testcase) (Server, *testSlackClient) {
+	testClient := testSlackClient{
+		test: test,
+	}
 	cl := httptest.NewServer(&testClient)
 	testClient.Client = cl.Client()
 	u, _ := url.Parse(cl.URL)
@@ -49,32 +72,31 @@ func NewMockServer()(Server, *testSlackClient){
 
 func server(client HttpClient) (Server, error) {
 	a, err := database.NewMap()
-	if err != nil{
+	if err != nil {
 		return Server{}, err
 	}
 	return NewServer("", "foobar", "", a, client), nil
 }
 
-type testSlackClient struct{
+type testSlackClient struct {
 	url *url.URL
 	*http.Client
-	messages []slack.WebhookMessage
+	message slack.WebhookMessage
+	test    testcase
 }
 
-func (t *testSlackClient)ServeHTTP(w http.ResponseWriter, r *http.Request){
-	switch r.URL.Path{
+func (t *testSlackClient) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
 	case "/api/oauth.v2.access":
 		a, _ := json.Marshal(slack.OAuthV2Response{
-			AccessToken: "1234",
-			TokenType:   "bot",
-			Team:        slack.OAuthV2ResponseTeam{
-				ID:   "id",
-				Name: "name",
+			AccessToken: "AccessToken",
+			TokenType:   "TokenType",
+			Team: slack.OAuthV2ResponseTeam{
+				ID: t.test.teamid,
 			},
 			IncomingWebhook: slack.OAuthResponseIncomingWebhook{
 				URL:       t.url.String(),
-				Channel:   "foobar",
-				ChannelID: "foobar",
+				ChannelID: t.test.channelid,
 			},
 			Enterprise:    slack.OAuthV2ResponseEnterprise{},
 			AuthedUser:    slack.OAuthV2ResponseAuthedUser{},
@@ -86,14 +108,12 @@ func (t *testSlackClient)ServeHTTP(w http.ResponseWriter, r *http.Request){
 		message := slack.WebhookMessage{}
 		b, _ := ioutil.ReadAll(r.Body)
 		json.Unmarshal(b, &message)
-		t.messages = append(t.messages, message)
+		t.message = message
 	}
 
 }
 
-
-
-func (t testSlackClient)Do(req *http.Request) (*http.Response, error){
+func (t testSlackClient) Do(req *http.Request) (*http.Response, error) {
 	req.URL.Host = t.url.Host
 	req.URL.Scheme = t.url.Scheme
 	req.Host = t.url.Host
